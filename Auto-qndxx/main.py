542fa03c5ff508b70fd11e190bae6105be168011
iCdo-X <102405245+iCdo-X@users.noreply.github.com>
2022-07-24 14:06:29 +0800

    Update main.py

--- Auto-qndxx/main.py
+++ Auto-qndxx/main.py
@@ -6,9 +6,6 @@
 
 import requests
 import re
-import smtplib
-from email.mime.text import MIMEText
-from email.utils import formataddr
 
 def auto_lean(openid,id):
     url = 'http://stu.redrock.team/api/course/studyCourse?openid='+str(openid)+'&id='+str(id)
@@ -44,34 +41,6 @@ def get_new_course_id(flag):
     else:
         return result['data'][0]['name']
     
-def send_mail(text,course_name,my_user):
-    my_sender = '2101543615@qq.com'  # 发件人邮箱账号
-    my_pass = 'emoxacplyrwdccaj'  # 发件人邮箱授权码，第一步得到的
-    ret = True
-    try:
-        # msg=MIMEText('填写邮件内容','plain','utf-8')
-        mail_msg = f"""
-                        <p>{course_name}{text}</p>
-                        <p>该交截图了!!!</p>
-                    """
-        msg = MIMEText(mail_msg, 'html', 'utf-8')
-        msg['From'] = formataddr(["托马斯提醒你", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
-        msg['To'] = formataddr(["呜呜呜", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
-        msg['Subject'] = "老年大学习来了"  # 邮件的主题，也可以说是标题
-        '''
-        QQ邮箱使用下面这种方式才成功
-        '''
-        # 发件人邮箱中的SMTP服务器，端口是25
-        # server=smtplib.SMTP("smtp.qq.com", 25)
-        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是465，固定的，不能更改 使用SSL模式
-        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱授权码
-        server.set_debuglevel(1)
-        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件信息
-        server.quit()  # 关闭连接
-    except Exception as err:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
-        print(err)
-        ret = False
-    return ret
 
 def main():
     '''
@@ -79,7 +48,7 @@ def main():
         * 由数字、大小写字母与符号组成的串
     '''
 #     微信OpenID
-    openid = [']
+    openid = ['']
 #     接受邮箱地址
     email_user = ['']
     # 获取最新的课程 Id   
